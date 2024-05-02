e7fe862bc91f3e7d3d3f78bde9b8a7dbe9006b29
Pavlos Ts <tsikrikaspavlos@gmail.com>
2024-05-02 14:55:10 +0300

    Second home page
    Buttons added as well

--- ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/HomeSecond.java
+++ ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/HomeSecond.java
@@ -3,30 +3,30 @@ package com.example.elderlypeopleui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class HomeSecond extends AppCompatActivity {
     ImageButton imgButton;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.top_view), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        imgButton = (ImageButton) findViewById(R.id.arrow_down);
+        imgButton = (ImageButton) findViewById(R.id.arrow_up);
 
         imgButton.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, HomeSecond.class);
+            Intent intent = new Intent(HomeSecond.this, MainActivity.class);
             startActivity(intent);
         });
     }
