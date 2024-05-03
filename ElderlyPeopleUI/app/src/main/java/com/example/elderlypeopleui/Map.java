2a2ebb2169b7ca8ab03c146b6545dfc0a99fc11e
AndreasP7 <papakitsosandreas@gmail.com>
2024-05-03 18:20:18 +0300

    Added toolbar, trans

--- ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Map.java
+++ ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Map.java
@@ -1,6 +1,32 @@
 package com.example.elderlypeopleui;
 
-public class Map {
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Map extends AppCompatActivity {
+
+    TextView title;
+    LinearLayout toolbar;
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.map);
+        title = findViewById(R.id.title);
+        title.setText("ΧΑΡΤΗΣ");
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.purple));
 
 
+
+    }
 }
