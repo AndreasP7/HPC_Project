bfcac100272a99b48b3a817babbbab4b27cd93f7
Pavlos Ts <tsikrikaspavlos@gmail.com>
2024-05-03 18:22:57 +0300

    minor changes

--- ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Phone.java
+++ ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Phone.java
@@ -1,23 +1,12 @@
 package com.example.elderlypeopleui;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Phone extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.phone);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.top_view), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
