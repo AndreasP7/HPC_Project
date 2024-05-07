dbe6559f825af15d15a2a15cc7bbeb4a0830be6a
AndreasP7 <papakitsosandreas@gmail.com>
2024-05-07 20:51:18 +0300

    Map Progress

--- ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Map.java
+++ ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Map.java
@@ -1,21 +1,36 @@
 package com.example.elderlypeopleui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Map extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     ImageButton imgButton;
     TextView title;
     LinearLayout toolbar;
+
+    private GoogleMap gMap;
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sos);
+        setContentView(R.layout.map);
         title = findViewById(R.id.title);
         title.setText("ΧΑΡΤΗΣ");
         toolbar = findViewById(R.id.toolbar);
@@ -27,5 +42,17 @@ public class Map extends AppCompatActivity {
             Intent intent = new Intent(Map.this, MainActivity.class);
             startActivity(intent);
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng destination = new LatLng(37.946487438528706, 23.66384436838045);
+        googleMap.addMarker(new MarkerOptions().position(destination).title("Home"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 18));
+
+    }
+
 }
