45b4fa09b5845b360915f371e6f78416fff788dd
Nikospapag <nikospapag2002@gmail.com>
2024-05-07 17:57:59 +0300

    ΠΑΙΡΝΟΥΜΕ CONFERENS
    ΠΑΙΡΝΟΥΜΕ CONFERENS

--- ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Weather.java
+++ ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Weather.java
@@ -1,4 +1,36 @@
 package com.example.elderlypeopleui;
 
-public class Weather {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Weather extends AppCompatActivity {
+    ImageButton imgButton;
+
+    TextView title;
+
+    LinearLayout toolbar;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather);
+        title = findViewById(R.id.title);
+        title.setText("ΚΑΙΡΟΣ ΣΗΜΕΡΑ");
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.blue));
+
+        imgButton = (ImageButton) findViewById(R.id.arrow_back);
+
+
+        imgButton.setOnClickListener(view -> {
+            Intent intent = new Intent(Weather.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
