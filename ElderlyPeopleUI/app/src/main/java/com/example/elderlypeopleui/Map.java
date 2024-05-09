c21d6ad47b0ce10c84f945fe01149b76a4244e52
Nikospapag <nikospapag2002@gmail.com>
2024-05-09 17:17:03 +0300

    current loc
    current loc

--- ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Map.java
+++ ElderlyPeopleUI/app/src/main/java/com/example/elderlypeopleui/Map.java
@@ -1,6 +1,7 @@
 package com.example.elderlypeopleui;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,25 +12,31 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
 
     ImageButton imgButton;
     TextView title;
     LinearLayout toolbar;
 
     private GoogleMap gMap;
-
+    private final int FINE_PERMISSION_CODE=1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -47,19 +54,48 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
             startActivity(intent);
         });
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
-        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
     }
 
+
+    private void getLastLocation(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>(){
+            public void onSuccess(Location location){
+                if(location != null){
+                    currentLocation = location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+                    mapFragment.getMapAsync(Map.this);
+                }
+            }
+        });
+    }
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        gMap = googleMap;
+        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(currentLatLng).title("My Location");
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18));
 
-        LatLng destination = new LatLng(37.946487438528706, 23.66384436838045);
+    }
 
-        gMap.addMarker(new MarkerOptions().position(destination).title("Σπιτι").icon(BitmapDescriptorFactory.fromResource(R.drawable.home)));
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 18));
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions , @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        if(requestCode== FINE_PERMISSION_CODE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
 
+            }else {
+                Toast.makeText(this,"Location permission is denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
